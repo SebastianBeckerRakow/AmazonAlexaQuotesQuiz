d6f462814c7de989dd62ecbb5cbf648ee16b61aa
Sebastian Becker-Rakow <SebastianBeckerRakow@users.noreply.github.com>
2017-01-18 14:18:18 +0100

    initial checkin

---
+++ src/questions.js
@@ -0,0 +1,66 @@
+﻿module.exports = [
+    {
+        "Man darf jetzt nicht alles so schlecht reden, wie es war.": [
+                "Fredi Bobic",
+                "Rudi Völler",
+                "Bruno Labbadia"
+            ],
+    },
+    {
+        "Ich brauche keinen Butler. Ich habe eine junge Frau!": [
+                "Thomas Doll",
+                "Lothar Matthäus",
+                "Mehmet Scholl"
+            ],
+    },
+    {
+        "Schiedsrichter kommt für mich nicht in Frage, schon eher etwas, das mit Fußball zu tun hat.": [
+                "Lothar Matthäus",
+                "Berti Voigts",
+                "Stefan Effenberg"
+            ],
+    },
+    {
+        "Ich bin abends mit der alten Dame Juve ins Bett gegangen und morgens wieder aufgestanden, weil ich perfekt informiert sein wollte.": [
+                "Jupp Heynckes",
+                "Uli Hoeneß",
+                "Horst Hrubesch"
+            ],
+    },
+    {
+        "Sie spielen taktisch gut, obwohl sie ohne Taktik spielen.": [
+                "Udo Lattek",
+                "Ottmar Hitzfeld",
+                "Jürgen Klinsmann"
+            ],
+    },
+    {
+        "Schlimm ist dieses Gejammer. Tut hier weh, tut da weh. Aber solange Sie das Handy halten können, muss ja noch genug Kraft da sein.": [
+                "Werner Lorant",
+                "Toni Polster",
+                "Peter Pacult"
+            ],
+    },
+    {
+        "Mailand oder Madrid - Hauptsache Italien.": [
+                "Andreas Möller",
+                "Lothar Mattäus",
+                "Thomas Strunz"
+            ],
+    },
+    {
+        "Mal verliert man und mal gewinnen die anderen.": [
+                "Otto Rehagel",
+                "Karl-Heinz Rummenigge",
+                "Olaf Thon"
+            ],
+    },
+    {
+        "Die Spieler waren schwach wie eine Flasche leer.": [
+                "Giovanni Trappatoni",
+                "Carlos Ancelotti",
+                "Thorsten Legat"
+            ],
+    },    
+];
+
